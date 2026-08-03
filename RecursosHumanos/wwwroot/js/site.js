e8b56b19c57e29e64fcd5d43f4be9b4b9004f7c7
Guadalupe Robles Garcia <grg091287@gmail.com>
2026-08-03 15:30:33 -0700

    Correcion de loading

--- RecursosHumanos/wwwroot/js/site.js
+++ RecursosHumanos/wwwroot/js/site.js
@@ -2,45 +2,32 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
-new tempusDominus.TempusDominus(
-    document.getElementById('fechaPicker'),
-    {
-        localization: {
-            locale: 'es'
-        },
-        display: {
-            components: {
-                calendar: true,
-                date: true,
-                month: true,
-                year: true,
-                decades: true,
-                clock: false
-            }
-        }
-    }
-);
 
 //Para formularios(POST)
 $(document).on("submit", "form", function () {
+    console.log("Submit")
     $("#loading").show();
 });
 
 //Para peticiones AJAX
 $(document).ajaxStart(function () {
+    console.log("Start")
     $("#loading").show();
 });
 
 $(document).ajaxStop(function () {
+    console.log("ajax hide")
     $("#loading").hide();
 });
 
 //Para ocultarlo al cargar la página
 $(window).on("load", function () {
+    console.log("Load")
     $("#loading").hide();
 });
 
 //todos los clics que provoquen una redirección
 $(document).on("click", "a", function () {
+    console.log("Click")
     $("#loading").show();
 });
